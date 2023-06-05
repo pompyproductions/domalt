997312a85836b9af47bb8c3c8b91ccf0fb5f8382
Eren Gazioglu <pompyproduction@gmail.com>
2023-06-06 00:40:46 +0200

    Add button to classes demo

--- demo/classes.js
+++ demo/classes.js
@@ -1,6 +1,9 @@
 import domalt from "../domalt.js";
 
 const entryPoint = document.getElementById("entry");
+const dye = () => {
+  document.getElementById("colorable").classList.toggle("pink");
+}
 
 entryPoint.append(
   domalt.newElem({
@@ -17,5 +20,17 @@ entryPoint.append(
     tag: "p",
     content: "The above element uses two classes and it works like you'd expect.",
     class: "up"
+  }),
+  domalt.newElem({
+    tag: "p",
+    content: "And here we use ID's to do things:",
+    id: "colorable"
+  }),
+  domalt.newElem({
+    tag: "button",
+    content: "Dye the text!",
+    id: "color-button"
   })
 );
+
+document.getElementById("color-button").addEventListener("click", dye);
