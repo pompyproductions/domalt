6cc28c092134ecdf3ac091c2895aec455121ce69
Eren Gazioglu <accounts@pompyproductions.com>
2025-03-17 18:31:49 +0100

    Global refactor

--- domalt.js
+++ domalt.js
@@ -1,9 +1,196 @@
 import { traverse } from "./modules/markdown.js";
 
 let uidCounter = 0;
+let defaultTag = "p";
+
 const savedElems = new Map();
 const collections = new Map();
 const templates = new Map();
+
+// ---
+// private helper methods
+
+function newElemFromArray(arr) {
+  return newElem({
+    tag: arr[0],
+    content: arr[1]
+  })
+}
+
+function newElemFromString(str) {
+  return newElem({
+    tag: defaultTag,
+    content: str
+  })
+}
+
+// ---
+// main newElem method
+
+function newElem(options = null) {
+  if (!options) return document.createElement("div");
+  if (typeof options === "string") return newElemFromString(options);
+  if (Array.isArray(options)) return newElemFromArray(options);
+
+  if (!Object.hasOwn(options, "tag")) {
+    options.tag = "div";
+  } else if (options.tag === "text") {
+    return options.content;
+  }
+
+  const elem = document.createElement(options.tag);
+
+  if (Object.hasOwn(options, "content")) {
+    if (options.allowInline) {
+      const nodes = traverse(options.content);
+      nodes.forEach((n) => elem.append(newElem(n)));
+    } else {
+      elem.textContent = options.content;
+    }
+  }
+
+  for (let key in options) {
+    const val = options[key];
+    switch (key) {
+      case "id":
+        elem.id = val;
+        break;
+
+      case "class":
+      case "className":
+        if (typeof val === "string") {
+          elem.classList.add(...val.split(" "))
+        } else if (Array.isArray(val)) {
+          elem.classList.add(...val.filter(item => item.length))
+        }
+        break;
+
+      case "attributes":
+        if (Array.isArray(val)) {
+          for (let attr of val) {
+            if (attr[0] === "data-uid") continue;
+            elem.setAttribute(attr[0], attr[1]);
+          }
+        } else {
+          for (let attr in val) {
+            if (attr === "data-uid") continue;
+            elem.setAttribute(attr, val[attr])
+          }
+        }
+        break;
+
+      case "children":
+        if (!val.length) break;
+        for (let node of val) {
+          if (node instanceof HTMLElement) {
+            elem.appendChild(node);
+          } else {
+            elem.appendChild(newElem(node));
+          }
+        }
+        break;
+
+      case "uid":
+        if (val === true) {
+          while (savedElems.has(uidCounter)) {
+            uidCounter++;
+          }
+          elem.setAttribute("data-domalt-id", uidCounter++);
+        }
+        break;
+
+      case "listeners":
+      case "eventListeners":
+        for (let listener of val) {
+          elem.addEventListener(listener[0], listener[1]);
+        }
+        break;
+
+      case "saveAs":
+        // overwrites if it exists
+        savedElems.set(val, elem);
+        break;
+
+      case "collection":
+        if (typeof val === "string") {
+          addToCollection(elem, val);
+        }
+        break;
+
+      case "style":
+        if (typeof val !== "object" || Array.isArray(val)) break;
+        Object.assign(elem.style, val);
+        break;
+    }
+  }
+  return elem;
+}
+
+
+// ---
+// derived newElem methods
+
+function newElemList(content, options = {}) {
+  // options: isOrdered, isNav
+  let listElems = [];
+  for (let item of content) {
+    if (typeof item === "object" || item instanceof HTMLElement) {
+      listElems.push(item);
+    } else {
+      listElems.push({ tag: "li", content: item });
+    }
+  }
+
+  if (options.isNav) {
+    return newElem(["nav", ])
+  }
+
+  return newElem({
+    tag: options.isOrdered ? "ol" : "ul",
+    children: listElems
+  });
+}
+
+function newElemNav(links, isOrdered = false) {
+  // links are: [textContent, href]
+  //
+  const navElems = [];
+  for (let link of links) {
+    navElems.push({
+      tag: "li",
+      children: [
+        {
+          tag: "a",
+          content: link[0],
+          attributes: [["href", link[1]]],
+        },
+      ],
+    });
+  }
+
+  return newElem({
+    tag: "nav",
+    children: [
+      {
+        tag: isOrdered ? "ol" : "ul",
+        children: navElems,
+      },
+    ],
+  });
+}
+
+function newElemDl(content) {
+  const elems = [];
+  for (let key of content) {
+    elems.push(
+      newElem()
+    )
+  }
+}
+
+// ---
+// save/load functionality
+
 
 function saveTemplate(name, obj) {
   templates.set(name, obj);
@@ -32,128 +219,8 @@ function retrieve(elemName) {
   return savedElems.get(elemName);
 }
 
-function newElem(obj) {
-  if (!Object.hasOwn(obj, "tag")) {
-    obj.tag = "div";
-  } else if (obj.tag === "text") {
-    return obj.content;
-  }
-  const elem = document.createElement(obj.tag);
-
-  if (Object.hasOwn(obj, "content")) {
-    if (obj.allowInline) {
-      const nodes = traverse(obj.content);
-      nodes.forEach((n) => elem.append(newElem(n)));
-    } else {
-      elem.textContent = obj.content;
-    }
-  }
-
-  for (let prop in obj) {
-    const val = obj[prop];
-    switch (prop) {
-      case "id":
-        elem.id = val;
-        break;
-      case "class":
-        elem.classList.add(...val.split(" "));
-        break;
-      case "attributes":
-        for (let attr of val) {
-          if (attr[0] === "data-uid") continue;
-          elem.setAttribute(attr[0], attr[1]);
-        }
-        break;
-      case "children":
-        if (!val.length) break;
-        for (let node of val) {
-          if (node instanceof HTMLElement) {
-            elem.appendChild(node);
-          } else {
-            elem.appendChild(newElem(node));
-          }
-        }
-        break;
-      case "uid":
-        if (val === true) {
-          while (savedElems.has(uidCounter)) {
-            uidCounter++;
-          }
-          elem.setAttribute("data-domalt-id", uidCounter++);
-        }
-        break;
-      case "listeners":
-        for (let listener of val) {
-          elem.addEventListener(listener[0], listener[1]);
-        }
-        break;
-      case "saveAs":
-        // overwrites if it exists
-        savedElems.set(val, elem);
-        break;
-      case "collection":
-        if (typeof val === "string") {
-          addToCollection(elem, val);
-        }
-        break;
-      case "style":
-        if (typeof val !== "object" || Array.isArray(val)) break;
-        Object.assign(elem.style, val);
-        break;
-    }
-  }
-  return elem;
+function setDefaultTag(tag) {
+  defaultTag = tag;
 }
 
-function newElemList(content, isOrdered = false) {
-  let listElems = [];
-  for (let item of content) {
-    if (typeof item === "object" || item instanceof HTMLElement) {
-      listElems.push(item);
-    } else {
-      listElems.push({ tag: "li", content: item });
-    }
-  }
-  // if (Array.isArray(obj.children)) {
-  //   obj.children.push(...)
-  // }
-  return newElem({
-    tag: isOrdered ? "ol" : "ul",
-    children: listElems
-  });
-}
-
-function newElemNav(links, isOrdered = false) {
-  // links are: [textContent, href]
-  const navElems = [];
-  for (let link of links) {
-    navElems.push({
-      tag: "li",
-      children: [
-        {
-          tag: "a",
-          content: link[0],
-          attributes: [["href", link[1]]],
-        },
-      ],
-    });
-  }
-  // for (let childNode of obj) {
-  //   if (childNode instanceof HTMLElement) {
-  //     children.push(childNode)
-  //   } else {
-  //     children.push(newElem(childNode))
-  //   }
-  // }
-  return newElem({
-    tag: "nav",
-    children: [
-      {
-        tag: isOrdered ? "ol" : "ul",
-        children: navElems,
-      },
-    ],
-  });
-}
-
-export default { newElem, newElemNav, newElemList, retrieve, retrieveCollection, saveTemplate, fromTemplate };
+export default { newElem, newElemNav, newElemList, retrieve, retrieveCollection, saveTemplate, fromTemplate, setDefaultTag };
